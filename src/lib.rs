533fd7d391f52a0a51a74870af12b06884afbf27
Jeff Porter <jeff.porter@acm.org>
2022-10-30 12:55:36 -0700

    Handle load error

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use rand::prelude::*;
-use std::future::Future;
+// use std::future::Future;
+use std::rc::Rc;
+use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 use web_sys::console;
@@ -97,17 +99,28 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
         let image = web_sys::HtmlImageElement::new().unwrap();
         let callback = Closure::once(move || {
             web_sys::console::log_1(&JsValue::from_str("loaded"));
-            success_tx.send(());
+
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                let _ = success_tx.send(Ok(()));
+            }
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                let _ = error_tx.send(Err(err));
+            }
         });
 
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
         image.set_src("Idle (1).png");
-        success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+        let _ = success_rx.await;
+        let _ = context.draw_image_with_html_image_element(&image, 0.0, 0.0);
 
         draw_sierpinski(&context, depth, [top, left, right]);
     });
